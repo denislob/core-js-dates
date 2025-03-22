27de6f81b75bc66546407122fed778d074180587
den <you@example.com>
2025-03-22 22:33:36 +0300

    feat: add solution to task getCountDaysOnPeriod.

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -116,8 +116,13 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const alpha = new Date(dateStart);
+  const omega = new Date(dateEnd);
+  const delta = omega - alpha;
+  const daysDelta = Math.ceil(delta / (1000 * 3600 * 24));
+  const countDaysOnPeriod = daysDelta + 1;
+  return countDaysOnPeriod;
 }
 
 /**
